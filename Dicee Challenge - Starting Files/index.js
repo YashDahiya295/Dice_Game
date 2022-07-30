5c7f065c92345c64a1f9cb5c7c34b7727b1c6b15
Yash Dahiya <106151257+YashDahiya295@users.noreply.github.com>
2022-07-30 10:18:49 +0530

    Add files via upload

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,33 @@
+var randomNumber1 = Math.random();
+randomNumber1 = randomNumber1 * 6;
+randomNumber1 = Math.floor(randomNumber1) + 1 ;
+
+var randomDiceImage = randomNumber1 + ".png" ;
+
+var randomImageSource = "images/" + randomDiceImage ;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src" , randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6 ) + 1 ;
+
+var randomImageSource2 = "images/" + randomNumber2 + ".png" ;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src" , randomImageSource2);
+
+// conditions for player to won the toss
+
+if(randomNumber1>randomNumber2)
+{
+  document.querySelector("h1").innerHTML=("♠︎ Player 1 Wins");
+}
+else if (randomNumber2>randomNumber1) {
+  document.querySelector("h1").innerHTML=("Player 2 Wins♠︎");
+}
+else
+{
+  document.querySelector("h1").innerHTML=("😂 Draw");
+}
